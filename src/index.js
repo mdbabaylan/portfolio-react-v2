f43b8a675eb25f3aaf056e46578e4632da213cbe
mdbabaylan <matmatmark@gmail.com>
2023-07-24 21:24:18 +0800

    front-page progress with svg images

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// let markObject = {
+//   name: "Mark Daniel C. Babaylan",
+
+//   softSkills: ["Customer Service", "Adaptability", "Accountability", 
+//   "Leadership", "Self-reliance", "Teamwork"],
+
+//   techSkils: ["React JS", "Express JS", "HTML/CSS/JS", "Solidity", "AWS S3/EB", "Mongo DB", 
+//   "MySQL/Microsoft SQL", "PHP/C#/Java/Android Dev"],
+
+//   message: "Adapt relentlessly and provide value"
+// }
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// console.log(markObject);
 root.render(
   <React.StrictMode>
     <App />
