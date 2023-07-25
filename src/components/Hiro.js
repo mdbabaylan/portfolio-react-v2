f4fa1e4d06bd5126175c891e9fc6c5eea678b8e2
mdbabaylan <matmatmark@gmail.com>
2023-07-25 11:23:15 +0800

    skills hook done, change honors to projects next

--- src/components/Hiro.js
+++ src/components/Hiro.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 
-import profile from '../assets/profile.svg';
+import profile from '../assets/code-snapshotv3.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCircleArrowRight, } from "@fortawesome/free-solid-svg-icons";
 import {  faGithub, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
@@ -20,7 +20,7 @@ export default function Hiro () {
             null
         }
         <div id="home" className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative">
-            <div className='md:w-3/6 md:p-4'>
+            <div className='md:w-4/5 md:p-4'>
                 <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)} />
             </div>
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
@@ -28,19 +28,16 @@ export default function Hiro () {
                     <h1 className="text-xl text-gray-400">Hi, I'm</h1>
                     <h1 className="text-2xl font-bold">Mark Babaylan</h1>
                     <p class="text-xl font-bold text-gray-300">Full Stack Developer</p>
-                    <p className="text-md font-light text-gray-400 ">Enthusiastic and motivated self-learning in Information Technology. Interested in exploring about Computer Network, Cloud Computing, DevSecops, and Web Development. Now I am looking for more experience in my field.</p>
+                    <p className="text-md font-light text-gray-400 ">Committed to positively contribute to the team and clients efficiently using what I have learned with my exposure
+                    from different industries paired with the drive to adapt and learn new skills & environments as needed.</p>
                 </div>
-                <a href='http://blog.dama.zip' className='mt-2 block'>Read My Blog <FontAwesomeIcon className='ml-2' icon={faCircleArrowRight}/> </a>
-                
+               
                 <ul className='flex mt-2 gap-4 items-center'>
                    <li>
-                        <a href='https://github.com/radendi' rel="noreferrer" target="_blank"><FontAwesomeIcon size='2xl' icon={faGithub} /></a>
+                        <a href='https://github.com/mdbabaylan' rel="noreferrer" target="_blank"><FontAwesomeIcon size='2xl' icon={faGithub} /></a>
                    </li> 
                     <li>
-                        <a href='https://instagram.com/damaasth' rel="noreferrer" target="_blank"><FontAwesomeIcon size='2xl' icon={faInstagram} /></a>
-                    </li>
-                    <li>
-                        <a href='htps://linkedin.com/damasukma' rel="noreferrer" target="_blank"><FontAwesomeIcon size='2xl' icon={faLinkedinIn} /></a>
+                        <a href='https://www.linkedin.com/in/mbabaylan' rel="noreferrer" target="_blank"><FontAwesomeIcon size='2xl' icon={faLinkedinIn} /></a>
                     </li>
                 </ul>
             </div>
